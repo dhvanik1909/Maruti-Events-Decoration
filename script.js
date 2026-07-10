e01921432d6d890608621f92ea4b1c0edab6c636
dhvanik1909 <dhvanik1909@gmail.com>
2026-07-10 20:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,3 +95,31 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll('.reveal').forEach(element => {
   observer.observe(element);
 });
+const counters=document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.dataset.target;
+const count=+counter.innerText;
+
+const increment=target/100;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+};
+
+update();
+
+});
